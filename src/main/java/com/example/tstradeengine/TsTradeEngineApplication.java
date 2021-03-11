c619a36c24d3944a735696910b3afce1d10ea938
penbosso <ckbosso@st.ug.edu.gh>
2021-03-11 15:53:25 +0000

    initial setup

---
+++ src/main/java/com/example/tstradeengine/TsTradeEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.tstradeengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsTradeEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsTradeEngineApplication.class, args);
+	}
+
+}
